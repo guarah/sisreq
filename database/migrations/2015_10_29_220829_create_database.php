e7272fc7546e33f981baeeb29c02e4539a7881c8
frrJunior <guara_st@hotmail.com>
2015-10-29 21:34:44 -0200

    criação do banco

--- database/migrations/2015_10_29_220829_create_database.php
+++ database/migrations/2015_10_29_220829_create_database.php
@@ -12,7 +12,21 @@ class CreateDatabase extends Migration
      */
     public function up()
     {
-        //
+        Schema::create('alunos', function (Blueprint $table) {
+            $table->increments('id_matricula');
+            $table->string('curso');
+            $table->timestamps();
+        });
+
+        Schema::create('requerimentos', function (Blueprint $table) {
+            $table->increments('id_requerimento');
+            $table->integer('id_aluno');
+            $table->foreign('id_aluno')->references('id_matricula')->on('alunos');
+            $table->string('nome');
+            $table->string('tipo');
+            $table->string('observacao');
+            $table->timestamps();
+        });
     }
 
     /**
@@ -22,6 +36,7 @@ class CreateDatabase extends Migration
      */
     public function down()
     {
-        //
+        Schema::drop('alunos');
+        Schema::drop('requerimentos');
     }
 }
